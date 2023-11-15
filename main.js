dd71cdebb531872293510c80bed3c6cb2816602f
Kelven Bruno Carlos Lopes Bulha (KB) <112175768+kelven939@users.noreply.github.com>
2023-11-15 20:47:12 +0200

    update

--- main.js
+++ main.js
@@ -1,17 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
     const playlist = document.getElementById('playlist');
     const audio = document.getElementById('audio');
+    const fileInput = document.getElementById('fileInput');
   
     // Verificar se há músicas no localStorage
     const storedSongs = JSON.parse(localStorage.getItem('songs')) || [];
   
     // Adicionar músicas ao playlist
     storedSongs.forEach((song, index) => {
-      const li = document.createElement('li');
-      li.textContent = `Música ${index + 1}`;
-      li.addEventListener('click', () => playSong(index));
+      const li = createListItem(index);
       playlist.appendChild(li);
     });
+  
+    function addSongs() {
+      const files = fileInput.files;
+      
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const song = { 
+          name: file.name,
+          url: URL.createObjectURL(file)
+        };
+  
+        storedSongs.push(song);
+        const li = createListItem(storedSongs.length - 1);
+        playlist.appendChild(li);
+      }
+  
+      localStorage.setItem('songs', JSON.stringify(storedSongs));
+    }
+  
+    function createListItem(index) {
+      const li = document.createElement('li');
+      li.textContent = storedSongs[index].name;
+      li.addEventListener('click', () => playSong(index));
+      return li;
+    }
   
     function playSong(index) {
       if (storedSongs.length > 0 && index >= 0 && index < storedSongs.length) {
